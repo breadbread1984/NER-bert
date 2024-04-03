c6c4a909dfc2ab17272a311793c213f0ce38f3df
breadbread1984 <breadbread1984@163.com>
2024-04-03 17:14:39 +0800

    update

--- predict.py
+++ predict.py
@@ -4,6 +4,7 @@ import json
 from os import getcwd
 from os.path import join, exists, abspath
 from absl import flags, app
+import adaseq
 from modelscope.pipelines import pipeline
 from modelscope.utils.constant import Tasks
 
@@ -12,10 +13,11 @@ FLAGS = flags.FLAGS
 def add_options():
   flags.DEFINE_string('input', default = None, help = 'input string')
   flags.DEFINE_string('ckpt', default = 'ckpt_ner', help = 'path to checkpoint directory')
-  flags.DEFINE_enum('device', default = 'cuda', enum_values = {'cuda', 'cpu'}, help = 'device to use')
+  flags.DEFINE_enum('device', default = 'gpu', enum_values = {'gpu', 'cpu'}, help = 'device to use')
 
 def main(unused_argv):
-  p = pipeline(Tasks.named_entity_recognition, abspath(FLAGS.ckpt))
+  print('load checkpoint: %s\n' % abspath(FLAGS.ckpt))
+  p = pipeline(Tasks.named_entity_recognition, abspath(FLAGS.ckpt), device = FLAGS.device)
   results = p(FLAGS.input)
   print(results)
 
